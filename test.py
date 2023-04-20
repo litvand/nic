f0c336830cbacc7c32591ec774d438ac999acdea
litvand <litv.andre@gmail.com>
2023-04-20 11:54:34 +0300

    Max fully connected, conv accuracy

--- test.py
+++ test.py
@@ -12,10 +12,14 @@ def print_accuracy(msg, outputs, labels):
 
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    _, (valid_imgs, valid_labels) = mnist.load_data(n_train=20000, n_valid=10000, device=device)
+    _, (valid_imgs, valid_labels) = mnist.load_data(n_train=20000, n_valid=5000, device=device)
     m = model.PoolNet(valid_imgs[0]).to(device)
-    model.load(m, 'pool-8315f5fc0646ced32418bc011f52a47c121edfcc.pt')
+    model.load(m, 'pool-bnorm-20k-faeca80e1ca4e0d35fe14157fdb4f02183d3d3cd.pt')
     m.eval()
     print_accuracy("Validation accuracy", m(valid_imgs[:10000]), valid_labels[:10000])
+
+
+# FullyConnected, 50k training images, 0.984 max validation accuracy
+# PoolNet BatchNorm, 20k training images, 0.99 max validation accuracy, 0.988 min validation accuracy
 
 
