a6ad867b9b630f9a0922b8437b335c221f021ffe
litvand <litv.andre@gmail.com>
2023-06-10 21:10:16 +0300

    _Automated commit

--- eval.py
+++ eval.py
@@ -52,7 +52,8 @@ def bin_acc(outputs, targets):
         on_pos.count_nonzero(),
         len(on_neg) - on_neg.count_nonzero(),
     )
-    print("len outputs, targets", len(outputs), len(targets), outputs)
+    print("len outputs, targets", len(outputs), len(targets), targets)
+    print("outputs[0]", outputs[0], outputs[0] == 0, outputs[0] == 1)
 
     balanced = (acc_on_pos + acc_on_neg) / 2
     return acc, balanced, acc_on_pos, acc_on_neg
